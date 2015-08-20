27e1128dc7b82b23eeda65f5656c7dc536d0274a
Valerie Chatelet <vlrchtlt@gmail.com>
2015-08-20 11:52:56 +0200

    add controller cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
+  get 'cocktails/delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
