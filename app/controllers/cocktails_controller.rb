2f8c9ed4a291c428298ab81819bd580160938589
Valerie Chatelet <vlrchtlt@gmail.com>
2015-08-20 14:13:54 +0200

    init controller cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,16 +1,31 @@
 class CocktailsController < ApplicationController
   def index
+    @coktails = Cocktail.all
   end
 
   def show
+    @cocktail
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
   end
 
   def delete
+    @cocktail.destroy
   end
+
+  private
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
 end
