c48a0c7e4191f207147509e6bbcd6423dfd0222a
Valerie Chatelet <vlrchtlt@gmail.com>
2015-08-21 15:59:57 +0200

    reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820151331) do
+ActiveRecord::Schema.define(version: 20150821130646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cocktails", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
     t.string   "picture"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   create_table "doses", force: :cascade do |t|
