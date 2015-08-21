b847fcc0ca842efbf19894c7e35c36cb63309b40
Valerie Chatelet <vlrchtlt@gmail.com>
2015-08-21 15:11:53 +0200

    form and strong params updated for cocktails in new view and in cocktailcontroller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -29,7 +29,7 @@ class CocktailsController < ApplicationController
 
   private
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :picture)
   end
 
   def set_cocktail
